a7cf46d1ae6b70c7004f29e3048573e1b3bba550
maksim <maxmiassspb@gmail.com>
2021-02-05 22:10:47 +0300

    nothing

--- weborganizer/weborganizer/settings.py
+++ weborganizer/weborganizer/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tasks/static/'),
-    os.path.join(BASE_DIR, 'weborganizer/static'),
+    os.path.join(BASE_DIR, 'weborganizer/static/'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
