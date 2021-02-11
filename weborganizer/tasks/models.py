28c868ad91d10296a891ecb8c64d6c1d8bf7a2f9
maksim <maxmiassspb@gmail.com>
2021-02-11 08:38:22 +0300

    Корректировка модели

--- weborganizer/tasks/models.py
+++ weborganizer/tasks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class CardTask(models.Model):
+
+    statuses = [('Task', 'Task'), ('Progress', 'Progress'), ('Complete', 'Complete'), ('Deleted', 'Deleted')]
+
+    title = models.CharField(verbose_name='Заголовок', max_length=150, blank=True, )
+    text = models.TextField(verbose_name='Описание задачи', blank=True, )
+    time_start = models.DateTimeField(verbose_name='Начало', blank=True)
+    time_end = models.DateTimeField(verbose_name='Завершение', blank=True)
+    task_status = models.CharField(verbose_name='Статус выполнения', max_length=8, default='Task', choices=statuses)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('edit_task', kwargs={'task_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        # ordering = ('-time_start')
