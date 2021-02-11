60f45d4e317cc923cc07147dc099c529d7079f7a
maksim <maxmiassspb@gmail.com>
2021-02-11 08:38:40 +0300

    Корректировка ссылок

--- weborganizer/tasks/urls.py
+++ weborganizer/tasks/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from tasks.views import *
+from .views import *
 
 
 urlpatterns = [
     path('', index, name='home'),
+    path('view/', view_tasks, name='view'),
+    path('add/', add_tasks, name='add'),
+    path('edit/', edit_tasks, name='edit'),
 
 ]
