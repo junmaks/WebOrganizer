f46297f90e51b20a6e4daf97abaeee5349683315
maksim <maxmiassspb@gmail.com>
2021-02-11 08:37:46 +0300

    Редактирование в админке

--- weborganizer/tasks/admin.py
+++ weborganizer/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import CardTask
+
+
+class CardTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'time_start', 'time_end', 'time_end', 'task_status', )
+    list_display_links = ('title', )
+    search_fields = ('title', 'task_status')
+    list_filter = ('time_start',)
+
+
+admin.site.register(CardTask, CardTaskAdmin)
